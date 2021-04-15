ba8c0c9870a603312f0d0aadb7046762c1ed9b98
Joshua Kosasih <joshua.2017tin013@civitas.ukrida.ac.id>
2021-04-15 17:46:28 +0700

    Challenge 2 Angular - Done

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { InputFormComponent } from './input-form/input-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
